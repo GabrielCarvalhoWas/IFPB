eacc289437baa36c593283885c255770ef6959ba
GabrielCarvalhoWas <pereira.carvalho@academico.ifpb.edu.br>
2024-07-04 14:25:55 -0300

    Update ex 04.py

--- revisao/ex 04.py
+++ revisao/ex 04.py
@@ -6,26 +6,27 @@ quantidadeCliente = 0
 quantidadeCuponsNaoDistribuido = 1000
 
 while quantidadeCuponsNaoDistribuido > 0:
-        cliente = float(input("Digite o valor da venda: "))
-        totalValor += cliente
-        if cliente < 30:
-            print("sem cupons.")
-            print(f"R$ {cliente:.2f} de saldo.")
-            print(f"R$ {30 - cliente:.2f} para novo cupom. ")
-        else:
-            cupons = cliente // 30
-            quantidadeCupons += cupons
-            saldo =  cliente % 30
-            print(f"{cupons:.2f} cupons")
-            print(f"{saldo:.2f} de saldo.")
-            print(f"{(30 - saldo):.2f} para novo cupom.")
-            if cliente > maiorValorVendido:
-                maiorValorVendido = cliente
-                cuponsEntregue = maiorValorVendido // 30
-
-            if saldo == 0:
-                quantidadeCliente += 1
-            
+        for vendas in range(200):
+                cliente = float(input("Digite o valor da venda: "))
+                totalValor += cliente
+                if cliente < 30:
+                    print("sem cupons.")
+                    print(f"R$ {cliente:.2f} de saldo.")
+                    print(f"R$ {30 - cliente:.2f} para novo cupom. ")
+                else:
+                    cupons = cliente // 30
+                    quantidadeCupons += cupons
+                    saldo =  cliente % 30
+                    print(f"{cupons:.2f} cupons")
+                    print(f"{saldo:.2f} de saldo.")
+                    print(f"{(30 - saldo):.2f} para novo cupom.")
+                    if cliente > maiorValorVendido:
+                        maiorValorVendido = cliente
+                        cuponsEntregue = maiorValorVendido // 30
+        
+                    if saldo == 0:
+                        quantidadeCliente += 1
+                    
 
 
 print(f"Total do valor vendido no dia: {totalValor}.")
